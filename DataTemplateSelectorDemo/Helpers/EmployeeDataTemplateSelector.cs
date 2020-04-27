a8874f1b4467ce8f074a28f539f0f4eae4976b24
Fons <Fons.Sonnemans@reflectionit.nl>
2020-04-27 20:41:50 +0200

    Add project files.

---
+++ DataTemplateSelectorDemo/Helpers/EmployeeDataTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using DataTemplateSelectorDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DataTemplateSelectorDemo.Helpers {
+    class EmployeeDataTemplateSelector : DataTemplateSelector {
+
+        public DataTemplate EvenTemplate { get; set; }
+        public DataTemplate OddTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container) {
+            if (item is Employee emp) {
+                return emp.Salary % 2 == 0 ? EvenTemplate : OddTemplate; 
+            }
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
---
+++ DataTemplateSelectorDemo/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTemplateSelectorDemo.Models {
+
+    internal class Employee {
+
+        public Employee() {
+        }
+
+        public Employee(string name, double salary) {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public string Name {
+            get;
+            set;
+        }
+
+        public double Salary {
+            get;
+            set;
+        }
+
+        public void RaiseSalary(double percentage) {
+            this.Salary += Salary * (percentage / 100);
+        }
+
+        public override string ToString() {
+            return string.Format("Employee Name = {0}, Salary = {1:C}", this.Name, this.Salary);
+        }
+    }
+
+}
